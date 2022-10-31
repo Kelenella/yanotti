d060276f0dedecb2ad35deb24f3cc7db12e1fc64
Jane Yushchenko <kelenella@ukr.net>
2022-10-31 20:36:38 +0200

    added logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,41 @@
 import s from "./Header.module.css";
+import logo from "../../img/logo-yanotti.jpg";
 
 export default function Header() {
   return (
     <>
-      <ul className={s.navigationList}>
-        <li className={s.navigationItem}>
-          <a href="/" className={s.navigationLink}>
-            Преимущества
-          </a>
-        </li>
-        <li className={s.navigationItem}>
-          <a href="/" className={s.navigationLink}>
-            Программа
-          </a>
-        </li>
-        <li className={s.navigationItem}>
-          <a href="/" className={s.navigationLink}>
-            Отзывы
-          </a>
-        </li>
-        <li className={s.navigationItem}>
-          <a href="/" className={s.navigationLink}>
-            Вопросы
-          </a>
-        </li>
-        <li className={s.navigationItem}>
-          <a href="/" className={s.navigationLink}>
-            Контакты
-          </a>
-        </li>
-      </ul>
+      <a href="/" className={s.logo}>
+        <img src={logo} alt="logo" width={150} />
+      </a>
+      <nav className={s.navigation}>
+        <ul className={s.navigationList}>
+          <li className={s.navigationItem}>
+            <a href="/" className={s.navigationLink}>
+              Преимущества
+            </a>
+          </li>
+          <li className={s.navigationItem}>
+            <a href="/" className={s.navigationLink}>
+              Программа
+            </a>
+          </li>
+          <li className={s.navigationItem}>
+            <a href="/" className={s.navigationLink}>
+              Отзывы
+            </a>
+          </li>
+          <li className={s.navigationItem}>
+            <a href="/" className={s.navigationLink}>
+              Вопросы
+            </a>
+          </li>
+          <li className={s.navigationItem}>
+            <a href="/" className={s.navigationLink}>
+              Контакты
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
