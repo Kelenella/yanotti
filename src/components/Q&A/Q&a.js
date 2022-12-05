b7f331e043ee279a517790e0f1dac218dcf19ea4
Jane Yushchenko <kelenella@ukr.net>
2022-12-05 18:47:59 +0200

    fixed QA section md and styles

--- src/components/Q&A/Q&a.js
+++ src/components/Q&A/Q&a.js
@@ -1,40 +1,60 @@
 import PropTypes from "prop-types";
+import img from "../../img/qa.jpg";
 
 import s from "./Q&a.module.css";
 
 const Qa = ({ title }) => {
   return (
     <section className={s.section}>
-      <h2 className={s.title} id="qa">
-        {title}
-      </h2>
-      <ul className={s.qaList}>
-        <li className={s.qaItem}>
-          <p className={s.question}>
-            Что мне понадобится для прохождения курса, кроме видео-уроков?
-          </p>
-        </li>
-        <li className={s.qaItem}>
-          <p className={s.question}>Сколько по времени длится массаж?</p>
-        </li>
-        <li className={s.qaItem}>
-          <p className={s.question}>Вызывает ли массажное масло прыщи?</p>
-        </li>
-        <li className={s.qaItem}>
-          <p className={s.question}>Возможен ли возврат после покупки?</p>
-        </li>
-        <li className={s.qaItem}>
-          <p className={s.question}>Когда я могу приступить к курсу?</p>
-        </li>
-        <li className={s.qaItem}>
-          <p className={s.question}>Когда я увижу результат?</p>
-        </li>
-        <li className={s.qaItem}>
-          <p className={s.question}>
-            Кому я могу задать дополнительные вопросы?
-          </p>
-        </li>
-      </ul>
+      <div className={s.wrapperQa}>
+        <h2 className={s.title} id="qa">
+          {title}
+        </h2>
+        <ul className={s.qaList}>
+          <li className={s.qaItem}>
+            <div>
+              <p className={s.question}>
+                Что мне понадобится для прохождения курса, кроме видео-уроков?
+              </p>
+            </div>
+          </li>
+          <li className={s.qaItem}>
+            <div>
+              <p className={s.question}>Сколько по времени длится массаж?</p>
+            </div>
+          </li>
+          <li className={s.qaItem}>
+            <div>
+              <p className={s.question}>Вызывает ли массажное масло прыщи?</p>
+            </div>
+          </li>
+          <li className={s.qaItem}>
+            <div>
+              <p className={s.question}>Возможен ли возврат после покупки?</p>
+            </div>
+          </li>
+          <li className={s.qaItem}>
+            <div>
+              <p className={s.question}>Когда я могу приступить к курсу?</p>
+            </div>
+          </li>
+          <li className={s.qaItem}>
+            <div>
+              <p className={s.question}>Когда я увижу результат?</p>
+            </div>
+          </li>
+          <li className={s.qaItem}>
+            <div>
+              <p className={s.question}>
+                Кому я могу задать дополнительные вопросы?
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <img src={img} alt="hands" />
+      </div>
     </section>
   );
 };
