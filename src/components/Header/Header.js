d72b786b08750bf88322eed18cc83698c12c9a84
Jane Yushchenko <kelenella@ukr.net>
2022-12-05 17:56:58 +0200

    fixed header and footer styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,56 +6,58 @@ import Icons from "../Icons/Icons";
 const Header = (props) => {
   return (
     <header className={s.header}>
-      <img src={logo} alt="logo" className={s.logo} />
-      <nav className={s.navigation}>
-        <ul className={s.navigationList}>
-          <li className={s.navigationItem}>
-            <a href="#helpfulness" className={s.navigationLink}>
-              Преимущества
-            </a>
-          </li>
-          <li className={s.navigationItem}>
-            <a href="#program" className={s.navigationLink}>
-              Программа
-            </a>
-          </li>
-          <li className={s.navigationItem}>
-            <a href="#reviews" className={s.navigationLink}>
-              Отзывы
-            </a>
-          </li>
-          <li className={s.navigationItem}>
-            <a href="#qa" className={s.navigationLink}>
-              Вопросы
-            </a>
-          </li>
-        </ul>
-        <ul className={s.contactsList}>
-          <li className={s.contactsItem}>
-            <a href="tel:+380993589253" className={s.contactsLink}>
-              +380993589253
-            </a>
-          </li>
-          <li className={s.contactsItem}>
-            <a
-              href="https://instagram.com/yanotti_massage?igshid=YmMyMTA2M2Y="
-              aria-label="YanottiOnInstagram"
-              className={s.contactsLink}
-            >
-              <Icons name="instagram" width={30} height={30} />
-            </a>
-          </li>
-          <li className={s.contactsItem}>
-            <a
-              href="/"
-              aria-label="YanottiOnYoutube"
-              className={s.contactsLink}
-            >
-              <Icons name="youtube" width={30} height={30} />
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <div className={s.headerWrapper}>
+        <img src={logo} alt="logo" className={s.logo} />
+        <nav className={s.navigation}>
+          <ul className={s.navigationList}>
+            <li className={s.navigationItem}>
+              <a href="#helpfulness" className={s.navigationLink}>
+                Преимущества
+              </a>
+            </li>
+            <li className={s.navigationItem}>
+              <a href="#program" className={s.navigationLink}>
+                Программа
+              </a>
+            </li>
+            <li className={s.navigationItem}>
+              <a href="#reviews" className={s.navigationLink}>
+                Отзывы
+              </a>
+            </li>
+            <li className={s.navigationItem}>
+              <a href="#qa" className={s.navigationLink}>
+                Вопросы
+              </a>
+            </li>
+          </ul>
+          <ul className={s.contactsList}>
+            <li className={s.contactsItem}>
+              <a href="tel:+380993589253" className={s.contactsLink}>
+                +380993589253
+              </a>
+            </li>
+            <li className={s.contactsItem}>
+              <a
+                href="https://instagram.com/yanotti_massage?igshid=YmMyMTA2M2Y="
+                aria-label="YanottiOnInstagram"
+                className={s.contactsLink}
+              >
+                <Icons name="instagram" width={30} height={30} />
+              </a>
+            </li>
+            <li className={s.contactsItem}>
+              <a
+                href="/"
+                aria-label="YanottiOnYoutube"
+                className={s.contactsLink}
+              >
+                <Icons name="youtube" width={30} height={30} />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,20 @@
 import Button from "../Button/Button";
+import Yana from "../../img/heroImg.jpg";
 import s from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <section className={s.section}>
-      <h2 className={s.subTitle}>Онлайн марафон</h2>
-      <div className={s.heroBlock}>
-        <h1 className={s.title}>делаем массаж лица вместе</h1>
-        <p className={s.text}>Хотите помолодеть за 10 дней на 5 лет?</p>
-        <Button text="Оставить заявку" type="submit" />
+      <div className={s.heroWrapper}>
+        <div>
+          <h2 className={s.subTitle}>Онлайн марафон</h2>
+          <h1 className={s.title}>делаем массаж лица вместе</h1>
+          <p className={s.text}>Хотите помолодеть за 10 дней на 5 лет?</p>
+          <Button text="Оставить заявку" type="submit" />
+        </div>
+        <div>
+          <img src={Yana} alt="Яна" />
+        </div>
       </div>
     </section>
   );
